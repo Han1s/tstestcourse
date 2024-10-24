e7112c80b1c7cf70c5df63f43711fd9eff7f3d1d
Jan Ranostaj <HonzaRanostaj@gmail.com>
2024-10-24 09:53:09 +0200

    9. Structure of the unit test

--- src/tests/Utils.test.ts
+++ src/tests/Utils.test.ts
@@ -1,8 +1,15 @@
 import { toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("should return uppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+  test("it should return uppercase of valid string", () => {
+    // arrange:
+    const sut = toUpperCase; // convention
+    const expected = "ABC";
+
+    // act
+    const actual = toUpperCase("abc");
+
+    // assert
+    expect(actual).toBe(expected);
   });
 });

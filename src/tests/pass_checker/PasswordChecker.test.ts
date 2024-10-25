b411d53d18c56d5e6079895288655c4d6fa33fc9
Jan Ranostaj <HonzaRanostaj@gmail.com>
2024-10-25 15:55:53 +0200

    25

--- src/tests/pass_checker/PasswordChecker.test.ts
+++ src/tests/pass_checker/PasswordChecker.test.ts
@@ -1,3 +1,4 @@
+import { PasswordErrors } from "./../../app/pass_checker/PasswordChecker";
 import { PasswordChecker } from "../../app/pass_checker/PasswordChecker";
 
 describe("PasswordChecker test suite", () => {
@@ -7,7 +8,43 @@ describe("PasswordChecker test suite", () => {
     sut = new PasswordChecker();
   });
 
-  it("should do nothing for the moment", () => {
-    sut.checkPassword();
+  it("Password with less than 8 chars is invalid", () => {
+    const actual = sut.checkPassword("1234567");
+    expect(actual.valid).toBe(false);
+    expect(actual.reasons).toContain(PasswordErrors.SHORT);
+  });
+
+  it("Password with more than 8 chars is ok", () => {
+    const actual = sut.checkPassword("123456Aa");
+    expect(actual.valid).toBe(true);
+    expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
+  });
+
+  it("Password with no upper case letter is invalid", () => {
+    const actual = sut.checkPassword("1234abcd");
+    expect(actual.valid).toBe(false);
+    expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
+  });
+
+  it("Password with an upper case letter is valid", () => {
+    const actual = sut.checkPassword("abcD");
+    expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
+  });
+
+  it("Password with no lower case letter is invalid", () => {
+    const actual = sut.checkPassword("1234ABCD");
+    expect(actual.valid).toBe(false);
+    expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE);
+  });
+
+  it("Password with a lower case letter is valid", () => {
+    const actual = sut.checkPassword("ABcD");
+    expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
+  });
+
+  it("Complex password is valid", () => {
+    const actual = sut.checkPassword("1234abcD");
+    expect(actual.reasons).toHaveLength(0);
+    expect(actual.valid).toBe(true);
   });
 });
